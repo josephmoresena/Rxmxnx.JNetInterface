2dfb74430ef7490076866b545d9de8e09a96ca98
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2023-07-09 21:48:40 -0500

    .

--- src/Intermediate/Rxmxnx.JNetInterface.Native.Intermediate/Native/JConstructorDefinition.cs
+++ src/Intermediate/Rxmxnx.JNetInterface.Native.Intermediate/Native/JConstructorDefinition.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// This class stores a constructor definition.
 /// </summary>
-public abstract record JConstructorDefinition : JCallDefinition
+public record JConstructorDefinition : JCallDefinition
 {
 	/// <inheritdoc/>
 	internal override Type? Return => default;
@@ -11,13 +11,51 @@ public abstract record JConstructorDefinition : JCallDefinition
 	/// <summary>
 	/// Constructor.
 	/// </summary>
-	internal JConstructorDefinition() : this(Array.Empty<JArgumentMetadata>()) { }
+	/// <remarks>This constructor should be never inherited.</remarks>
+	public JConstructorDefinition() : this(Array.Empty<JArgumentMetadata>()) { }
+
 	/// <summary>
 	/// Constructor.
 	/// </summary>
-	internal JConstructorDefinition(params JArgumentMetadata[] metadata) 
+	protected JConstructorDefinition(params JArgumentMetadata[] metadata) 
 		: base(JCallDefinition.ConstructorName, metadata) { }
-	
+
+	/// <summary>
+	/// Creates a new <see cref="JLocalObject"/> instance using a constructor on <paramref name="jClass"/> 
+	/// which matches with current definition passing the default value for each argument.
+	/// </summary>
+	/// <param name="jClass">A <see cref="JClassObject"/> instance.</param>
+	/// <returns>A new <see cref="JLocalObject"/> instance.</returns>
+	public JLocalObject New(JClassObject jClass) => this.New<JLocalObject>(jClass, this.CreateArgumentsArray());
+	/// <summary>
+	/// Creates a new <typeparamref name="TObject"/> instance using a constructor which matches with
+	/// current definition passing the default value for each argument.
+	/// </summary>
+	/// <param name="env"><see cref="IEnvironment"/> instance.</param>
+	/// <returns>A new <typeparamref name="TObject"/> instance.</returns>
+	public TObject New<TObject>(IEnvironment env) 
+		where TObject : JLocalObject, IDataType<TObject>
+		=> this.New<TObject>(env.ClassProvider.GetClass<TObject>(), this.CreateArgumentsArray());
+
+	/// <summary>
+	/// Creates a new <see cref="JLocalObject"/> instance using a constructor on <paramref name="jClass"/> 
+	/// which matches with current definition.
+	/// </summary>
+	/// <param name="jClass">A <see cref="JClassObject"/> instance.</param>
+	/// <param name="args">The arguments to pass to.</param>
+	/// <returns>A new <see cref="JLocalObject"/> instance.</returns>
+	protected JLocalObject New(JClassObject jClass, IObject?[] args) => this.New<JLocalObject>(jClass, args);
+	/// <summary>
+	/// Creates a new <typeparamref name="TObject"/> instance using a constructor which matches with
+	/// current definition.
+	/// </summary>
+	/// <param name="env"><see cref="IEnvironment"/> instance.</param>
+	/// <param name="args">The arguments to pass to.</param>
+	/// <returns>A new <typeparamref name="TObject"/> instance.</returns>
+	protected TObject New<TObject>(IEnvironment env, IObject?[] args) 
+		where TObject : JLocalObject, IDataType<TObject>
+		=> this.New<TObject>(env.ClassProvider.GetClass<TObject>(), args);
+
 	/// <summary>
 	/// Creates a new <typeparamref name="TObject"/> instance using a constructor on <paramref name="jClass"/> 
 	/// which matches with current definition.
@@ -26,48 +64,10 @@ public abstract record JConstructorDefinition : JCallDefinition
 	/// <param name="jClass">A <see cref="JClassObject"/> instance.</param>
 	/// <param name="args">The arguments to pass to.</param>
 	/// <returns>A new <typeparamref name="TObject"/>.</returns>
-	internal TObject New<TObject>(JClassObject jClass, IObject?[] args)
+	private TObject New<TObject>(JClassObject jClass, IObject?[] args)
 		where TObject : JLocalObject, IDataType<TObject>
 	{
 		IEnvironment env = jClass.Environment;
 		return env.Accessor.CallConstructor<TObject>(jClass, this, args);
 	}
-}
-
-/// <summary>
-/// This class stores a constructor definition.
-/// </summary>
-/// <typeparam name="TObject"><see cref="IDataType"/> type of constructor.</typeparam>
-public record JConstructorDefinition<TObject> : JConstructorDefinition
-	where TObject : JLocalObject, IDataType<TObject>
-{
-	/// <summary>
-	/// Constructor.
-	/// </summary>
-	/// <remarks>This constructor should be never inherited.</remarks>
-	public JConstructorDefinition() : this(Array.Empty<JArgumentMetadata>()) { }
-	
-	/// <summary>
-	/// Constructor.
-	/// </summary>
-	protected JConstructorDefinition(params JArgumentMetadata[] metadata) : base(metadata) { }
-	
-	/// <summary>
-	/// Creates a new <typeparamref name="TObject"/> instance using a constructor which matches with
-	/// current definition passing the default value for each argument.
-	/// </summary>
-	/// <param name="env"><see cref="IEnvironment"/> instance.</param>
-	/// <returns>A new <typeparamref name="TObject"/> instance.</returns>
-	public TObject New(IEnvironment env) 
-		=> base.New<TObject>(env.ClassProvider.GetClass<TObject>(), this.CreateArgumentsArray());
-
-	/// <summary>
-	/// Creates a new <typeparamref name="TObject"/> instance using a constructor which matches with
-	/// current definition.
-	/// </summary>
-	/// <param name="env"><see cref="IEnvironment"/> instance.</param>
-	/// <param name="args">The arguments to pass to.</param>
-	/// <returns>A new <typeparamref name="TObject"/>.</returns>
-	protected TObject New(IEnvironment env,IObject?[] args) 
-		=> base.New<TObject>(env.ClassProvider.GetClass<TObject>(), args);
 }
