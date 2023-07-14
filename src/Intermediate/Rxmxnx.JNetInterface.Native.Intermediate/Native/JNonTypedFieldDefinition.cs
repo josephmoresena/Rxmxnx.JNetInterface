354ba07a7df8936ae7959884184b8abe0b494a69
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2023-07-14 17:48:54 -0500

    Update JNonTypedFieldDefinition.cs

--- src/Intermediate/Rxmxnx.JNetInterface.Native.Intermediate/Native/JNonTypedFieldDefinition.cs
+++ src/Intermediate/Rxmxnx.JNetInterface.Native.Intermediate/Native/JNonTypedFieldDefinition.cs
@@ -15,26 +15,42 @@ public sealed record JNonTypedFieldDefinition : JFieldDefinition
 	/// <param name="signature">Signature field.</param>
 	public JNonTypedFieldDefinition(CString name, CString signature) 
 		: base(name, JAccessibleObjectDefinition.ValidateSignature(signature)) { }
-	
-	/// <inheritdoc cref="JFieldDefinition{TField}.Get(JLocalObject)"/>
+
+	/// <summary>
+	/// Retrieves the value of a field on <paramref name="jLocal"/> which matches with current definition.
+	/// </summary>
+	/// <param name="jLocal">A <see cref="JLocalObject"/> instance.</param>
+	/// <returns>The <paramref name="jLocal"/> field's value.</returns>
 	public JLocalObject? Get(JLocalObject jLocal)
 	{
 		IEnvironment env = jLocal.Environment;
 		return env.Accessor.GetField<JLocalObject>(jLocal, this);
 	}
-	/// <inheritdoc cref="JFieldDefinition{TField}.StaticGet(JClassObject)"/>
+	/// <summary>
+	/// Retrieves the value of a static field on <paramref name="jClass"/> which matches with current definition.
+	/// </summary>
+	/// <param name="jClass">A <see cref="JClassObject"/> instance.</param>
+	/// <returns>The <paramref name="jClass"/> field's value.</returns>
 	public JLocalObject? StaticGet(JClassObject jClass)
 	{
 		IEnvironment env = jClass.Environment;
 		return env.Accessor.GetStaticField<JLocalObject>(jClass, this);
 	}
-	/// <inheritdoc cref="JFieldDefinition{TField}.Set(JLocalObject, TField?)"/>
+	/// <summary>
+	/// Sets the value of a field on <paramref name="jLocal"/> which matches with current definition.
+	/// </summary>
+	/// <param name="jLocal">A <see cref="JLocalObject"/> instance.</param>
+	/// <param name="value">The field value to set to.</param>
 	public void Set(JLocalObject jLocal, JLocalObject? value)
 	{
 		IEnvironment env = jLocal.Environment;
 		env.Accessor.SetField(jLocal, this, value);
 	}
-	/// <inheritdoc cref="JFieldDefinition{TField}.StaticSet(JClassObject, TField?)"/>
+	/// <summary>
+	/// Sets the value of a static field on <paramref name="jClass"/> which matches with current definition.
+	/// </summary>
+	/// <param name="jClass">A <see cref="JClassObject"/> instance.</param>
+	/// <param name="value">The field value to set to.</param>
 	public void StaticSet(JClassObject jClass, JLocalObject? value)
 	{
 		IEnvironment env = jClass.Environment;
