d65a379418606218ac597c42434c9bb6a0108ef8
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2023-08-03 00:25:44 -0500

    .

--- src/Intermediate/Rxmxnx.JNetInterface.Base.Intermediate/Types/Inheritance/IDerivedType.cs
+++ src/Intermediate/Rxmxnx.JNetInterface.Base.Intermediate/Types/Inheritance/IDerivedType.cs
@@ -6,6 +6,7 @@ namespace Rxmxnx.JNetInterface.Types.Inheritance;
 /// <typeparam name="TDerivative">Type of a datatype which is derived of <typeparamref name="TBase"/>.</typeparam>
 /// <typeparam name="TBase">Type of datatype which <typeparamref name="TDerivative"/> extends or implements.</typeparam>
 [UnconditionalSuppressMessage("Trim analysis", "IL2091")]
+[EditorBrowsable(EditorBrowsableState.Never)]
 public interface IDerivedType<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.Interfaces)] out TDerivative,
 	TBase> : IReferenceType<TDerivative> where TDerivative : JReferenceObject, IReferenceType<TDerivative>
 	where TBase : JReferenceObject, IReferenceType<TBase>
--- src/Intermediate/Rxmxnx.JNetInterface.Base.Intermediate/Types/JTypeKind.cs
+++ src/Intermediate/Rxmxnx.JNetInterface.Base.Intermediate/Types/JTypeKind.cs
@@ -1,4 +1,4 @@
-namespace Rxmxnx.JNetInterface;
+namespace Rxmxnx.JNetInterface.Types;
 
 /// <summary>
 /// Kinds of java datatype.
