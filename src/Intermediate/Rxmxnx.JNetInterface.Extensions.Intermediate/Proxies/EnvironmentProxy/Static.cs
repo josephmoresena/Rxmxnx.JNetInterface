76e8362998bd721df768a14bd0beb5a6ee5869e7
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2024-02-06 17:02:32 -0500

    Update Static.cs

--- src/Intermediate/Rxmxnx.JNetInterface.Extensions.Intermediate/Proxies/EnvironmentProxy/Static.cs
+++ src/Intermediate/Rxmxnx.JNetInterface.Extensions.Intermediate/Proxies/EnvironmentProxy/Static.cs
@@ -6,9 +6,8 @@ public abstract partial class EnvironmentProxy
 	/// Creates a <see cref="JClassObject"/> for <c>java.lang.Class&lt;?&gt;</c> data type.
 	/// </summary>
 	/// <param name="env">A <see cref="EnvironmentProxy"/> instance.</param>
-	/// <param name="classRef">A <see cref="JClassLocalRef"/> reference.</param>
 	/// <returns>A <see cref="JClassObject"/> for <c>java.lang.Class&lt;?&gt;</c> data type.</returns>
-	public static JClassObject CreateClassObject(EnvironmentProxy env, JClassLocalRef classRef = default) => new(env);
+	public static JClassObject CreateClassObject(EnvironmentProxy env) => new(env);
 	/// <summary>
 	/// Creates a <see cref="JLocalObject"/> instance.
 	/// </summary>
@@ -16,31 +15,4 @@ public abstract partial class EnvironmentProxy
 	/// <param name="localRef">A <see cref="JObjectLocalRef"/> reference.</param>
 	/// <returns>A <see cref="JLocalObject"/> instance.</returns>
 	public static JLocalObject CreateObject(JClassObject jClass, JObjectLocalRef localRef) => new(jClass, localRef);
-	/// <summary>
-	/// Creates a <typeparamref name="TObject"/> instance.
-	/// </summary>
-	/// <param name="jClass">A <see cref="JClassObject"/> instance.</param>
-	/// <param name="localRef">A <see cref="JObjectLocalRef"/> reference.</param>
-	/// <typeparam name="TObject"></typeparam>
-	/// <returns>A <typeparamref name="TObject"/> instance.</returns>
-	public static TObject CreateObject<TObject>(JClassObject jClass, JObjectLocalRef localRef)
-		where TObject : JLocalObject, IReferenceType<TObject>
-	{
-		JReferenceTypeMetadata metadata = IReferenceType.GetMetadata<TObject>();
-		return (TObject)metadata.CreateInstance(jClass, localRef, true);
-	}
-	/// <summary>
-	/// Creates a <typeparamref name="TObject"/> instance.
-	/// </summary>
-	/// <param name="env">A <see cref="EnvironmentProxy"/> instance.</param>
-	/// <param name="localRef">A <see cref="JObjectLocalRef"/> reference.</param>
-	/// <typeparam name="TObject"></typeparam>
-	/// <returns>A <typeparamref name="TObject"/> instance.</returns>
-	public static TObject CreateObject<TObject>(EnvironmentProxy env, JObjectLocalRef localRef)
-		where TObject : JLocalObject, IReferenceType<TObject>
-	{
-		JReferenceTypeMetadata metadata = IReferenceType.GetMetadata<TObject>();
-		JClassObject jClass = env.GetClass<TObject>();
-		return (TObject)metadata.CreateInstance(jClass, localRef);
-	}
 }
