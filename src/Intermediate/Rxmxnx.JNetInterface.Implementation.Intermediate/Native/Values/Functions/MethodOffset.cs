7c03196a0e704ca9ad162b6eab94853b0899137b
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2024-05-27 01:01:00 -0500

    .

--- src/Intermediate/Rxmxnx.JNetInterface.Implementation.Intermediate/Native/Values/Functions/MethodOffset.cs
+++ src/Intermediate/Rxmxnx.JNetInterface.Implementation.Intermediate/Native/Values/Functions/MethodOffset.cs
@@ -5,7 +5,11 @@ namespace Rxmxnx.JNetInterface.Native.Values.Functions;
 /// </summary>
 [InlineArray(2)]
 [StructLayout(LayoutKind.Sequential)]
+[SuppressMessage(CommonConstants.CSharpSquid, CommonConstants.CheckIdS1144,
+                 Justification = CommonConstants.BinaryStructJustification)]
 internal record struct MethodOffset
 {
+#pragma warning disable CS0169
 	private IntPtr _reserved0;
+#pragma warning restore CS0169
 }
