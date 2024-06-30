89acfb055c446e8f5cd276c77e1ff5470896c842
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2024-06-29 23:37:37 -0500

    Fix field

--- src/Test/Rxmxnx.JNetInterface.Implementation.Tests.Proxies/InvokeInterfaceHelper/Fields.cs
+++ src/Test/Rxmxnx.JNetInterface.Implementation.Tests.Proxies/InvokeInterfaceHelper/Fields.cs
@@ -2,15 +2,17 @@ namespace Rxmxnx.JNetInterface.Tests;
 
 public static unsafe partial class InvokeInterfaceHelper
 {
-	private static readonly InternalInvokeInterface invokeInterface = new()
-	{
-		DestroyVirtualMachine = &InvokeInterfaceHelper.DestroyVirtualMachine,
-		AttachCurrentThread = &InvokeInterfaceHelper.AttachCurrentThread,
-		DetachCurrentThread = &InvokeInterfaceHelper.DetachCurrentThread,
-		AttachCurrentThreadAsDaemon = &InvokeInterfaceHelper.AttachCurrentThreadAsDaemon,
-		GetEnv = &InvokeInterfaceHelper.GetEnv,
-	};
+	private static readonly InternalInvokeInterface[] invokeInterface = [
+		new()
+		{
+			DestroyVirtualMachine = &InvokeInterfaceHelper.DestroyVirtualMachine,
+			AttachCurrentThread = &InvokeInterfaceHelper.AttachCurrentThread,
+			DetachCurrentThread = &InvokeInterfaceHelper.DetachCurrentThread,
+			AttachCurrentThreadAsDaemon = &InvokeInterfaceHelper.AttachCurrentThreadAsDaemon,
+			GetEnv = &InvokeInterfaceHelper.GetEnv,
+		},
+	];
 	private static readonly ConcurrentDictionary<JVirtualMachineRef, InvokeInterfaceProxy> proxies = new();
 	private static readonly MemoryHelper helper =
-		new(NativeUtilities.GetUnsafeValPtr(InvokeInterfaceHelper.invokeInterface), Int16.MaxValue);
+		new((IntPtr)InvokeInterfaceHelper.invokeInterface.AsMemory().Pin().Pointer, Int16.MaxValue);
 }
