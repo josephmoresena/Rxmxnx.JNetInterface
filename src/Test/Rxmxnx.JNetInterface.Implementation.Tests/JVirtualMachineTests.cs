d0d7fa9640d995c3848d492a677e873687d43da2
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2024-07-11 00:15:24 -0500

    Update JVirtualMachineTests.cs

--- src/Test/Rxmxnx.JNetInterface.Implementation.Tests/JVirtualMachineTests.cs
+++ src/Test/Rxmxnx.JNetInterface.Implementation.Tests/JVirtualMachineTests.cs
@@ -12,7 +12,7 @@ public sealed class JVirtualMachineTests
 		{
 			if (JVirtualMachineTests.factory.TryGetTarget(out ProxyFactory? result))
 				return result;
-			result = new(20, 20);
+			result = new(50, 50);
 			JVirtualMachineTests.factory.SetTarget(result);
 			return result;
 		}
