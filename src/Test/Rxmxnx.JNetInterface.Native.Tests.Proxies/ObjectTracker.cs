8842098df4c6833b83eef93f429502a1eff17a0d
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2024-07-28 16:19:11 -0500

    Update ObjectTracker.cs

--- src/Test/Rxmxnx.JNetInterface.Native.Tests.Proxies/ObjectTracker.cs
+++ src/Test/Rxmxnx.JNetInterface.Native.Tests.Proxies/ObjectTracker.cs
@@ -3,6 +3,6 @@ namespace Rxmxnx.JNetInterface.Tests;
 [ExcludeFromCodeCoverage]
 public sealed class ObjectTracker
 {
-	public WeakReference WeakReference { get; init; }
+	public WeakReference WeakReference { get; init; } = new(null);
 	public IWrapper<Boolean>? FinalizerFlag { get; init; }
 }
