ef017e8f264dbcae4e6a3384e2ebf628d13e14fb
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2024-11-02 23:45:18 -0500

    Update FeatureSwitches.cs

--- src/Intermediate/Rxmxnx.JNetInterface.Implementation.Intermediate/JVirtualMachine/FeatureSwitches.cs
+++ src/Intermediate/Rxmxnx.JNetInterface.Implementation.Intermediate/JVirtualMachine/FeatureSwitches.cs
@@ -1,6 +1,6 @@
 namespace Rxmxnx.JNetInterface;
 
-public partial class JVirtualMachine : IVirtualMachine
+public partial class JVirtualMachine
 {
 	/// <summary>
 	/// Indicates whether metadata for built-in throwable objects should be auto-registered.
