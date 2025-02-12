f8dbe2322f000638d25140d663a9cd4627cfd910
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2025-02-12 10:08:13 -0500

    Trace when no.reflection (tmp)

--- src/ApplicationTest/Rxmxnx.JNetInterface.LauncherTest/TestCompiler/CompileNetArgs.cs
+++ src/ApplicationTest/Rxmxnx.JNetInterface.LauncherTest/TestCompiler/CompileNetArgs.cs
@@ -27,6 +27,7 @@ public partial class TestCompiler
 			args.Add($"/p:NativeAOT={compileArgs.Publish.HasFlag(Publish.NativeAot)}");
 			args.Add($"/p:IlcDisableReflection={compileArgs.Publish.HasFlag(Publish.NoReflection)}");
 			args.Add($"/p:CopyTargetTo={compileArgs._outputPath}");
+			args.Add($"/p:USE_JTRACE={compileArgs.Publish.HasFlag(Publish.NoReflection)}");
 		}
 	}
 }
