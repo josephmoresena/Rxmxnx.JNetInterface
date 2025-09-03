2b83966f9952f3347e220ba3a6a8ae4b9a0dcad3
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2025-09-03 08:21:45 -0500

    Revert "Update InvocationFunctionSet.cs"
    This reverts commit 34fe6d8d9185a2e06ffc106af7deb5ecefc992b8.

--- src/Intermediate/Rxmxnx.JNetInterface.Implementation.Intermediate/Native/Values/Functions/InvocationFunctionSet.cs
+++ src/Intermediate/Rxmxnx.JNetInterface.Implementation.Intermediate/Native/Values/Functions/InvocationFunctionSet.cs
@@ -14,46 +14,16 @@ internal readonly unsafe struct InvocationFunctionSet
 	/// Pointer to <c>JNI_GetDefaultJavaVMInitArgs</c> exported function.
 	/// Returns a default configuration for the Java VM.
 	/// </summary>
-	private readonly delegate* unmanaged<void*, Int32> _getDefaultVirtualMachineInitArgs;
+	public readonly delegate* unmanaged<ref VirtualMachineInitArgumentValue, JResult> GetDefaultVirtualMachineInitArgs;
 	/// <summary>
 	/// Pointer to <c>JNI_CreateJavaVM</c> exported function.
 	/// Loads and initializes a Java VM. The current thread becomes the main thread.
 	/// </summary>
-	private readonly delegate* unmanaged<void*, void*, void*, Int32> _createVirtualMachine;
+	public readonly delegate* unmanaged<out JVirtualMachineRef, out JEnvironmentRef, in VirtualMachineInitArgumentValue,
+		JResult> CreateVirtualMachine;
 	/// <summary>
 	/// Pointer to <c>JNI_GetCreatedJavaVMs</c> exported function.
 	/// Returns all Java VMs that have been created.
 	/// </summary>
-	public readonly delegate* unmanaged<void*, Int32, Int32*, Int32> _getCreatedVirtualMachines;
-
-	/// <summary>
-	/// <c>JNI_GetDefaultJavaVMInitArgs</c>.
-	/// </summary>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public JResult GetDefaultVirtualMachineInitArgs(ref VirtualMachineInitArgumentValue initArg)
-	{
-		fixed (VirtualMachineInitArgumentValue* initArgPtr = &initArg)
-			return (JResult)this._getDefaultVirtualMachineInitArgs(initArgPtr);
-	}
-	/// <summary>
-	/// <c>JNI_CreateJavaVM</c>.
-	/// </summary>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public JResult CreateVirtualMachine(out JVirtualMachineRef vmRef, out JEnvironmentRef envRef,
-		in VirtualMachineInitArgumentValue initArg)
-	{
-		fixed (JVirtualMachineRef* vmRefPtr = &vmRef)
-		fixed (JEnvironmentRef* envRefPtr = &envRef)
-		fixed (VirtualMachineInitArgumentValue* initArgPtr = &initArg)
-			return (JResult)this._createVirtualMachine(vmRefPtr, envRefPtr, initArgPtr);
-	}
-	/// <summary>
-	/// <c>JNI_GetCreatedJavaVMs</c>.
-	/// </summary>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public JResult GetCreatedVirtualMachines(JVirtualMachineRef* arr, Int32 arrSize, out Int32 count)
-	{
-		fixed (Int32* coutPtr = &count)
-			return (JResult)this._getCreatedVirtualMachines(arr, arrSize, coutPtr);
-	}
+	public readonly delegate* unmanaged<JVirtualMachineRef*, Int32, out Int32, JResult> GetCreatedVirtualMachines;
 }
