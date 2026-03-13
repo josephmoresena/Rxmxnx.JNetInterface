9eb9bbae9130abae0f4c82957e61fdbcb5f94c0f
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2026-03-13 08:46:54 -0500

    Revert "Remove FFI"
    This reverts commit e4d8f5dae4b87ae29ef50126b5f399b5f17deb78.

---
+++ src/Test/Rxmxnx.JNetInterface.FFI.Tests/JVirtualMachineLibraryTests/Static.cs
@@ -0,0 +1,56 @@
+namespace Rxmxnx.JNetInterface.Tests;
+
+public sealed unsafe partial class JVirtualMachineLibraryTests
+{
+	private const String NativeLibraryName = "JvmProxy";
+
+	private static readonly String systemLibraryName = SystemInfo.IsWindows ? "user32" :
+		SystemInfo.IsMac ? "/usr/lib/libSystem.B.dylib" : "libc";
+	private static readonly String libraryPrefix = SystemInfo.IsWindows ? String.Empty : "lib";
+	private static readonly String nativeExtension = SystemInfo.IsWindows ? ".dll" :
+		SystemInfo.IsMac ? ".dylib" : ".so";
+	private static readonly IFixture fixture = new Fixture().RegisterReferences();
+
+	private static String GetProxyPath(JvmProxyType proxyType)
+	{
+		StringBuilder fileNameBuilder = new(50);
+		fileNameBuilder.Append(JVirtualMachineLibraryTests.libraryPrefix);
+		fileNameBuilder.Append(JVirtualMachineLibraryTests.NativeLibraryName);
+		if (proxyType is not JvmProxyType.Complete) fileNameBuilder.Append($".{proxyType}");
+		fileNameBuilder.Append($".{RuntimeInformation.ProcessArchitecture}".ToLowerInvariant());
+		fileNameBuilder.Append(JVirtualMachineLibraryTests.nativeExtension);
+		return fileNameBuilder.ToString();
+	}
+	private static CStringSequence CreateOptionsSequence()
+	{
+		Span<Int32> sizes = stackalloc Int32[4];
+		sizes[0] = Random.Shared.Next(0, 5);
+		sizes[1] = Random.Shared.Next(0, 5);
+		sizes[2] = Random.Shared.Next(0, 5);
+		sizes[3] = Random.Shared.Next(0, 5);
+		return new(JVirtualMachineLibraryTests.fixture.CreateMany<String>(sizes[0])
+		                                      .Concat(Enumerable.Repeat(String.Empty, sizes[1]))
+		                                      .Concat(JVirtualMachineLibraryTests.fixture.CreateMany<String>(sizes[2]))
+		                                      .Concat(Enumerable.Repeat(String.Empty, sizes[3])));
+	}
+	private static ReadOnlyValPtr<VirtualMachineInitOptionValue> GetOptionsPtr(
+		Span<VirtualMachineInitOptionValue> optionsAlloc, CStringSequence options)
+	{
+		Int32 index = 0;
+		foreach (ReadOnlySpan<Byte> value in options.CreateView())
+		{
+			optionsAlloc[index] = new(value.GetUnsafeValPtr());
+			index++;
+		}
+		return optionsAlloc.GetUnsafeValPtr();
+	}
+	private static delegate* unmanaged<FuncPtr<GetDefaultVirtualMachineInitArgsDelegate>,
+		FuncPtr<CreateVirtualMachineDelegate>, FuncPtr<GetCreatedJavaVMsDelegate>, void> GetProxyMethods(
+			JVirtualMachineLibrary library, out delegate* unmanaged<void> reset)
+	{
+		reset = (delegate* unmanaged<void>)NativeLibrary.GetExport(library.Handle, "Reset");
+		return (delegate* unmanaged< FuncPtr<GetDefaultVirtualMachineInitArgsDelegate>,
+			FuncPtr<CreateVirtualMachineDelegate>, FuncPtr<GetCreatedJavaVMsDelegate>, void>)NativeLibrary.GetExport(
+			library.Handle, "ArrangeInvocation");
+	}
+}
---
+++ src/Test/Rxmxnx.JNetInterface.FFI.Tests/JVirtualMachineLibraryTests/ThreadStatic.cs
@@ -0,0 +1,208 @@
+namespace Rxmxnx.JNetInterface.Tests;
+
+public sealed unsafe partial class JVirtualMachineLibraryTests
+{
+	[ThreadStatic]
+	private static Int32 count;
+	[ThreadStatic]
+	private static Int32 jniVersion;
+	[ThreadStatic]
+	private static JVirtualMachineInitArg? args;
+	[ThreadStatic]
+	private static JResult result;
+	[ThreadStatic]
+	private static NativeInterfaceProxy? proxyEnv;
+	[ThreadStatic]
+	private static ReadOnlyValPtr<VirtualMachineInitOptionValue> optionsPtr;
+	[ThreadStatic]
+	private static NativeInterfaceProxy[]? proxies;
+
+	private static JResult GetDefaultVirtualMachineInitArgsForGetLatestSupportedVersionTest(
+		ref VirtualMachineInitArgumentValue initValue)
+	{
+		try
+		{
+#if NET8_0_OR_GREATER
+			ref readonly VirtualMachineInitOptionValue options =
+#else
+			ref VirtualMachineInitOptionValue options =
+#endif
+				ref Unsafe.AsRef<VirtualMachineInitOptionValue>(initValue.Options);
+
+			if (JVirtualMachineLibraryTests.count == 0)
+				Assert.Equal((Int32)JRuntimeVersion.SEd2, initValue.Version);
+			JVirtualMachineLibraryTests.count++;
+#if NET8_0_OR_GREATER
+			Assert.True(Unsafe.IsNullRef(in options));
+#else
+			Assert.True(Unsafe.IsNullRef(ref options));
+#endif
+			Assert.Equal(default, initValue.OptionsLength);
+			Assert.Equal(default, initValue.IgnoreUnrecognized);
+
+			if (initValue.Version > JVirtualMachineLibraryTests.jniVersion)
+				return JResult.VersionError;
+
+			initValue = new()
+			{
+				Version = initValue.Version,
+				OptionsLength = 0,
+				Options = ReadOnlyValPtr<VirtualMachineInitOptionValue>.Zero,
+				IgnoreUnrecognized = JVirtualMachineLibraryTests.fixture.Create<Boolean>(),
+			};
+			return JResult.Ok;
+		}
+		catch (Exception ex)
+		{
+			LibraryBaseTests.NativeException = ex;
+			return JResult.Ok;
+		}
+	}
+	private static JResult CreateVirtualMachineForCreateVirtualMachineTest(out JVirtualMachineRef vmRef,
+		out JEnvironmentRef envRef, in VirtualMachineInitArgumentValue value)
+	{
+		if (JVirtualMachineLibraryTests.proxyEnv is null || JVirtualMachineLibraryTests.args is null)
+		{
+			vmRef = default;
+			envRef = default;
+			return JResult.DetachedThreadError;
+		}
+		try
+		{
+#if NET8_0_OR_GREATER
+			ref readonly VirtualMachineInitOptionValue options =
+#else
+			ref VirtualMachineInitOptionValue options =
+#endif
+				ref Unsafe.AsRef<VirtualMachineInitOptionValue>(value.Options);
+			Boolean nullOptions =
+#if NET8_0_OR_GREATER
+				Unsafe.IsNullRef(in options);
+#else
+				Unsafe.IsNullRef(ref options);
+#endif
+
+			Assert.Equal((Int32)JRuntimeVersion.J6, value.Version);
+			Assert.Equal(JVirtualMachineLibraryTests.args.Options.NonEmptyCount, value.OptionsLength);
+			Assert.Equal(JVirtualMachineLibraryTests.args.IgnoreUnrecognized, value.IgnoreUnrecognized);
+			if (!nullOptions && (ReadOnlyValPtr<Byte>)options.OptionString is { IsZero: false, } optionString)
+			{
+#if NET8_0_OR_GREATER
+				ref readonly Byte optionsBuffer =
+#else
+				ref Byte optionsBuffer =
+#endif
+					ref Unsafe.AsRef(in JVirtualMachineLibraryTests.args.Options.GetPinnableReference());
+#if NET8_0_OR_GREATER
+				Assert.True(Unsafe.AreSame(in optionsBuffer, in optionString.Reference));
+#else
+				Assert.True(Unsafe.AreSame(ref optionsBuffer, ref Unsafe.AsRef(in optionString.Reference)));
+#endif
+			}
+			else
+			{
+				Assert.Equal(0, value.OptionsLength);
+			}
+
+			if (JVirtualMachineLibraryTests.result != JResult.Ok)
+			{
+				vmRef = default;
+				envRef = default;
+				return JVirtualMachineLibraryTests.result;
+			}
+
+			vmRef = JVirtualMachineLibraryTests.proxyEnv.VirtualMachine.Reference;
+			envRef = JVirtualMachineLibraryTests.proxyEnv.Reference;
+			return JResult.Ok;
+		}
+		catch (Exception ex)
+		{
+			LibraryBaseTests.NativeException = ex;
+			vmRef = default;
+			envRef = default;
+			return JVirtualMachineLibraryTests.result;
+		}
+	}
+	private static JResult GetDefaultVirtualMachineInitArgsForGetDefaultArgumentTest(
+		ref VirtualMachineInitArgumentValue initValue)
+	{
+		if (JVirtualMachineLibraryTests.args is null) return JResult.DetachedThreadError;
+		try
+		{
+#if NET8_0_OR_GREATER
+			ref readonly VirtualMachineInitOptionValue options =
+#else
+			ref VirtualMachineInitOptionValue options =
+#endif
+				ref Unsafe.AsRef<VirtualMachineInitOptionValue>(initValue.Options);
+			Boolean nullOptions =
+#if NET8_0_OR_GREATER
+				Unsafe.IsNullRef(in options);
+#else
+				Unsafe.IsNullRef(ref options);
+#endif
+
+			Assert.InRange(initValue.Version, (Int32)JRuntimeVersion.SEd2,
+			               Math.Max(JVirtualMachineLibraryTests.args.Version, (Int32)JRuntimeVersion.SEd2));
+			Assert.True(nullOptions);
+			Assert.Equal(default, initValue.OptionsLength);
+			Assert.Equal(default, initValue.IgnoreUnrecognized);
+
+			initValue = new()
+			{
+				Version = initValue.Version,
+				OptionsLength = JVirtualMachineLibraryTests.args.Options.Count,
+				Options = JVirtualMachineLibraryTests.optionsPtr,
+				IgnoreUnrecognized = JVirtualMachineLibraryTests.args.IgnoreUnrecognized,
+			};
+			return JVirtualMachineLibraryTests.result;
+		}
+		catch (Exception ex)
+		{
+			LibraryBaseTests.NativeException = ex;
+			return JVirtualMachineLibraryTests.result;
+		}
+	}
+	private static JResult GetCreatedJavaVMs(ValPtr<JVirtualMachineRef> vmBuf, Int32 bufLen, out Int32 nVMs)
+	{
+		if (JVirtualMachineLibraryTests.proxies is null)
+		{
+			nVMs = 0;
+			return JResult.DetachedThreadError;
+		}
+		try
+		{
+			nVMs = JVirtualMachineLibraryTests.proxies.Length;
+			JVirtualMachineLibraryTests.count++;
+
+			if (JVirtualMachineLibraryTests.count == 1)
+			{
+				Assert.True(Unsafe.IsNullRef(ref vmBuf.Reference));
+				Assert.Equal(0, bufLen);
+				return JResult.InvalidArgumentsError;
+			}
+
+			Span<JVirtualMachineRef> vmRefs = MemoryMarshal.CreateSpan(ref vmBuf.Reference, bufLen);
+			Assert.Equal(JVirtualMachineLibraryTests.proxies.Length, vmRefs.Length);
+			JVirtualMachineLibraryTests.proxies.Select(p => p.VirtualMachine.Reference).ToArray().CopyTo(vmRefs);
+			nVMs = vmRefs.Length;
+			return JVirtualMachineLibraryTests.result;
+		}
+		catch (Exception ex)
+		{
+			LibraryBaseTests.NativeException = ex;
+			nVMs = -1;
+			return JVirtualMachineLibraryTests.result;
+		}
+	}
+	private static void CleanUp()
+	{
+		LibraryBaseTests.NativeException = default;
+		JVirtualMachineLibraryTests.count = default;
+		JVirtualMachineLibraryTests.jniVersion = default;
+		JVirtualMachineLibraryTests.result = default;
+		JVirtualMachineLibraryTests.proxyEnv = default;
+		JVirtualMachineLibraryTests.optionsPtr = default;
+		JVirtualMachineLibraryTests.proxies = default;
+	}
+}
