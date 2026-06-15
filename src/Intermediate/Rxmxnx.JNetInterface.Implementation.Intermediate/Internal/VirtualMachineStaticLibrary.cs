263209e602e2c7640da0743e8f72bb39a53258aa
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2026-06-14 19:09:43 -0500

    Update VirtualMachineStaticLibrary.cs

--- src/Intermediate/Rxmxnx.JNetInterface.Implementation.Intermediate/Internal/VirtualMachineStaticLibrary.cs
+++ src/Intermediate/Rxmxnx.JNetInterface.Implementation.Intermediate/Internal/VirtualMachineStaticLibrary.cs
@@ -20,7 +20,7 @@ internal abstract class VirtualMachineStaticLibrary : IVirtualMachineLibraryType
 	public static extern JResult CreateVirtualMachine(out JVirtualMachineRef vmRef, out JEnvironmentRef envRef,
 		in VirtualMachineInitArgumentValue initArg);
 	/// <inheritdoc/>
-	[DllImport("*", EntryPoint = IVirtualMachineLibraryType.CreateVirtualMachineSymbol)]
+	[DllImport("*", EntryPoint = IVirtualMachineLibraryType.GetCreatedVirtualMachinesSymbol)]
 	public static extern JResult GetCreatedVirtualMachines(ValPtr<JVirtualMachineRef> arr, Int32 arrSize,
 		out Int32 count);
 #pragma warning restore SYSLIB1054
